8631b53579b99e0308fdb235baca28c6412727be
Eric Devine <e.devine@live.com>
2021-04-12 14:26:46 -0400

    Initialize

---
+++ Branding/International Generosity/Marketing Templates/setlink.js
@@ -0,0 +1,8 @@
+//Change the URL below to edit the location that the logo on the form "clicks" to
+const URL = "https://www.trustbridgeglobal.com/";
+
+
+//...But don't touch this
+function setHREF(){
+  this.href=URL;
+}
